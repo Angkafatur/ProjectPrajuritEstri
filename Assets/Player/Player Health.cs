5bb1a2e3dc35cc98a610e7a0c2040607f2271b5c
Angka Fatur <106165938+Angkafatur@users.noreply.github.com>
2024-05-13 16:25:08 +0700

    Player
    add script player health

---
+++ Assets/Player/Player Health.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public float health;
+    public float maxHealth;
+    public Image healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
