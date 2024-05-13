dd221fdf8af0366c5a84bed1959f1bf66d733396
Angka Fatur <106165938+Angkafatur@users.noreply.github.com>
2024-05-13 17:44:09 +0700

    Player + Enemy
    add script player arrow
    add script player bow
    add script enemy health
    add sprite renderer enemy

---
+++ Assets/Player/Arrow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public int damage = 20;
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        enemy enemy = hitInfo.GetComponent<enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+
+}
